404e56db6c9485a3a011232080a7014c989addd0
sunil parmar <sunil04ce@gmail.com>
2023-09-19 23:16:28 +0530

    started shop list in stores

--- ec/phone_pe/src/screens/StoreStyles.js
+++ ec/phone_pe/src/screens/StoreStyles.js
@@ -2,139 +2,33 @@ import { StyleSheet } from 'react-native'
 import { moderateScale, moderateVerticalScale, scale, verticalScale } from 'react-native-size-matters'
 
 const styles = StyleSheet.create({
-    updateCard: {
+    searchBox: {
         width: '94%',
-        borderRadius: moderateScale(5),
+        height: verticalScale(45),
+        backgroundColor: 'white',
         alignSelf: 'center',
         marginTop: moderateVerticalScale(10),
-        backgroundColor: 'white',
-        // shadowRadius: 10,
-        // shadowColor: 'rgba(0,0,0,0.5)',
-        // shadowOffset: { x: 0, y: 1 }
-    },
-    updateTopView: {
+        borderRadius: moderateScale(30),
         flexDirection: 'row',
-        width: '100%',
+        alignItems: 'center',
         paddingLeft: moderateScale(15),
         paddingRight: moderateScale(15),
-        marginTop: moderateVerticalScale(20)
     },
-    logo: {
-        width: scale(30),
-        height: scale(30)
+    searchIcon: {
+        width: scale(18),
+        height: scale(18)
     },
-    updateTitle: {
-        fontSize: moderateScale(18),
-        fontWeight: '600',
-        marginLeft: moderateScale(10)
-    },
-    updateMsg: {
-        fontSize: moderateScale(14),
-        width: '70%',
-        marginLeft: moderateScale(10),
-        color: '#6e6e6e'
-    },
-    updateBtnView: {
-        alignSelf: 'flex-end',
-        flexDirection: 'row',
-        marginTop: moderateVerticalScale(20),
-        marginBottom: moderateVerticalScale(15),
-        alignItems: 'center',
-        marginRight: moderateScale(20)
-    },
-    later: {
-        color: 'purple',
-        fontWeight: '600',
-        fontSize: moderateScale(16)
-    },
-    update: {
-        backgroundColor: 'purple',
-        paddingBottom: moderateScale(10),
-        paddingTop: moderateScale(10),
-        paddingLeft: moderateScale(20),
-        paddingRight: moderateScale(20),
-        borderRadius: moderateScale(20),
-        marginLeft: moderateScale(20),
-    },
-    updateText: {
-        color: 'white',
-        fontWeight: '600',
-        fontSize: moderateScale(16)
-    },
-    banner: {
-        width: '94%',
-        height: verticalScale(100),
-        alignSelf: 'center',
-        marginTop: moderateVerticalScale(20),
-        borderRadius: moderateScale(10)
-    },
-    moneyTransferCard: {
-        width: '94%',
-        height: verticalScale(150),
-        backgroundColor: 'white',
-        alignSelf: 'center',
-        marginTop: moderateVerticalScale(10),
-        borderRadius: moderateScale(10)
-    },
-    heading: {
-        fontSize: moderateScale(16),
-        fontWeight: '600',
+    searchPlaceholder: {
+        color: '#9e9e9e',
         marginLeft: moderateScale(15),
-        marginTop: moderateVerticalScale(15)
+        fontSize: moderateScale(15),
     },
-    transferView: {
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        marginTop: moderateScale(20)
-    },
-    transferTab: {
-        width: '25%',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    transferCard: {
-        width: scale(36),
-        height: scale(36),
-        borderRadius: moderateScale(10),
-        backgroundColor: 'purple',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    transferText: {
-        marginTop: moderateScale(5),
-        textAlign: 'center'
-    },
-    otherOptions: {
-        flexDirection: 'row',
+    shopItem:
+    {
         width: '94%',
-        marginTop: moderateScale(15),
-        marginBottom: moderateScale(5),
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        alignSelf: 'center',
-    },
-    otherOptionTab: {
-        width: '31%',
-        height: verticalScale(80),
-        backgroundColor: '#277be8',
-        borderRadius: moderateScale(18),
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    otherOptionText: {
-        color: 'white',
-        fontWeight: '600',
-        marginTop: moderateScale(10),
-    },
-    rechargeAndBills: {
         backgroundColor: 'white',
-        borderRadius: moderateScale(5),
-        marginTop: moderateVerticalScale(10),
         alignSelf: 'center',
-        height: verticalScale(270),
-        width: '94%'
+        marginTop: moderateVerticalScale(20)
     }
 });
 
--- ec/phone_pe/src/screens/Stores.js
+++ ec/phone_pe/src/screens/Stores.js
@@ -1,4 +1,4 @@
-import { View, Text, StatusBar, ScrollView } from 'react-native'
+import { View, Text, StatusBar, ScrollView, Image, FlatList } from 'react-native'
 import React from 'react'
 import CommonHeader from './CommonHeader'
 import { styles } from './StoreStyles';
@@ -11,7 +11,17 @@ const Stores = () => {
             <StatusBar barStyle={'light-content'} />
             <CommonHeader />
             <ScrollView style={{ marginBottom: moderateScale(75) }}>
+                <View style={styles.searchBox}>
+                    <Image source={require('../images/search.png')} style={styles.searchIcon} />
+                    <Text style={styles.searchPlaceholder}>Search by store name or phone number</Text>
+                </View>
+                <FlatList data={[1, 1, 1, 1, 1]} renderItem={({ item, index }) => {
+                    return (
+                        <View style={styles.shopItem}>
 
+                        </View>
+                    )
+                }}></FlatList>
             </ScrollView>
         </View>
     )
