09ea71e953c630fa584bb85d16931b5f0af45ccd
sunil parmar <sunil04ce@gmail.com>
2023-10-21 00:09:16 +0530

    changed ndk gradle, started initial code

--- ec/reanimated/App.tsx
+++ ec/reanimated/App.tsx
@@ -1,10 +1,22 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
+// import Animated from 'react-native-reanimated';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{flex: 1}}>
+      <View style={{height: 100, width: 100, backgroundColor: 'red'}}></View>
+      <TouchableOpacity
+        style={{
+          width: '90%',
+          height: 50,
+          backgroundColor: 'black',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 200,
+        }}>
+        <Text style={{color: 'white'}}>Scale View</Text>
+      </TouchableOpacity>
     </View>
   );
 };
