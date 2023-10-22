86af97a94307c134296aa73b660a2f3acb64b0be
sunil parmar <sunil04ce@gmail.com>
2023-10-22 22:28:06 +0530

    change

--- sa/ecommerce_app/api/index.js
+++ sa/ecommerce_app/api/index.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const crypto = require('crypto');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
 
 const app = express();
 const port = 8000;
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-mongoose.connect("mongodb+srv://sunil04ce:sa_ecom_app@cluster0.llrokwm.mongodb.net/", {
+mongoose
+  .connect("mongodb+srv://sunil04ce:<password>@cluster0.llrokwm.mongodb.net/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.log('Error connecting to MongoDB', err);
-});
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
 
 app.listen(port, () => {
-    console.log('Server is running on port : ', port);
-})
+  console.log("Server is running on port : ", port);
+});
